54026300ef069c3157176dc9c5f325df5a50bd95
Kim Montemayor <46793374+kimmontemayor@users.noreply.github.com>
2019-03-25 13:48:29 -0400

    Changed to read from score files & send to client
    Done in collaboration with Michael and Tyler

--- Server.java
+++ Server.java
@@ -1,75 +1,30 @@
 import java.io.*;
 import java.net.*;
-import java.util.Date;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.stage.Stage;
 
-public class Server extends Application {
 
-  TextArea ta = new TextArea();
+public class Server {
 
-  @Override
-  public void start(Stage primaryStage) {
+//  TextArea ta = new TextArea();
+  DataOutputStream outputToClient = null;
 
-    // Create a scene and place it in the stage
-    Scene scene = new Scene(new ScrollPane(ta), 450, 200);
-    primaryStage.setTitle("Server");
-    primaryStage.setScene(scene);
-    primaryStage.show();
+  public void readFile(String scores){
+    try{
+      ServerSocket serverSocket = new ServerSocket(8000);
+      Socket socket = serverSocket.accept();
+      BufferedReader reader = new BufferedReader(new FileReader(scores));
+      outputToClient = new DataOutputStream(socket.getOutputStream());
+      String line = reader.readLine();
 
-    new Thread( () -> {
-      try {
-        //Creating socket server
-    	  ServerSocket serverSocket = new ServerSocket(8000);
-
-        Platform.runLater(() -> ta.appendText("Server started at " + new Date() + '\n'));
-
-        while (true) {
-          Socket socket = serverSocket.accept();
-          new Thread(new HandleAClient(socket)).start();
-        }
+      while(line != null){
+        outputToClient.writeUTF(line);
+        line = reader.readLine();
       }
-      catch(IOException ex) {
-        ex.printStackTrace();
-      }
-    }).start();
-  }
 
-  //Handle multiple clients
-  class HandleAClient implements Runnable {
-    private Socket socket;
+      reader.close();
+    }catch(FileNotFoundException e){
+      System.out.println("File not found");
+    }catch(IOException e){
 
-    public HandleAClient(Socket socket) {
-      this.socket = socket;
     }
-
-    public void run() {
-      try {
-
-        DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-        DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-
-        while (true) {
-          //read score from clent
-          int score = inputFromClient.readInt();
-
-          //display final score on server
-          Platform.runLater(() -> {
-            ta.appendText("Your score is: " + score + "\n");
-          });
-        }
-      }
-      catch(IOException ex) {
-        ex.printStackTrace();
-      }
-    }
-  }
-
-  public static void main(String[] args) {
-    launch(args);
   }
 }
